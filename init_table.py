c96e1b8987ba02280ac925cac7f00e1e8783c698
bzfy <65746870+bzfy@users.noreply.github.com>
2024-10-16 19:36:05 +1100

    Update init_table.py

--- init_table.py
+++ init_table.py
@@ -1,5 +1,6 @@
 import lorem
-from datetime import datetime
+import random
+from datetime import datetime, timedelta
 
 
 def generate_product_insert_sql():
@@ -22,7 +23,30 @@ def generate_product_insert_sql():
 
     return "\n".join(sql_statements)
 
+def generate_order_insert_sql():
+    sql_statements = []
+    location = [f'p{i}' for i in range(1, 11)]
+    account_ids = [str(i) for i in range(1, 11)]
+    for i in range(1, 11):
+        order_id = f'o{i}'
+        buyer_account_id = random.choice(account_ids)
+        seller_account_id = random.choice(account_ids)
+        product_id = f'p{seller_account_id}'
+        purchased_date = datetime.now() - timedelta(random.randint(0, 30))
+        booking_time = purchased_date + timedelta(hours=random.randint(1, 48))
+        meetup_location = f"Location {i}"
+
+
+        sql = f"INSERT INTO orders (order_id, buyer_account_id, seller_account_id, product_id, purchased_date, booking_time, meetup_location) VALUES " \
+              f"('{order_id}', '{buyer_account_id}', '{seller_account_id}', '{product_id}', '{purchased_date.isoformat()}', '{booking_time.isoformat()}', '{meetup_location}') ON CONFLICT (order_id) DO NOTHING;"
+        
+        sql_statements.append(sql)
+    
+    return "\n".join(sql_statements)
+
+
+
 def init_table():
-    sql_script = generate_product_insert_sql()
+    sql_script = (generate_product_insert_sql() + '\n' + generate_order_insert_sql())
     with open("init_products.sql", "w") as file:
         file.write(sql_script)
